cb50bb6c6c342c32d27380fc6f094d6e2349da24
Chris Krycho <hello@chriskrycho.com>
2020-02-21 16:26:31 -0700

    feature(Octane): remove all blueprint files
    The new Octane-ready blueprints will work by transforming the output of
    the default generators: by renaming the file from `.js` to `.ts` and by
    transforming the AST for files where necessary (e.g. adding a reference
    to a registry). The net is that we will no longer use standalone files
    for these, which in turn means we will have much less work to do to stay
    in sync with the upstream.
    
    (It also means that if we come to a point where the upstream so desires,
    it will be able to run tests to see if changes there break this repo!)

--- blueprints/component/module-unification-files/__root__/__path__/component.ts
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-
-export default class <%= classifiedModuleName %> extends Component.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  // normal class body definition here
-};
