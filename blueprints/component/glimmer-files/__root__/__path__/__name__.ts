be2f7c177af770665bcec94da34fc1334dfd299e
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2020-11-08 19:41:00 +0000

    add helpful comment in component args

--- blueprints/component/glimmer-files/__root__/__path__/__name__.ts
+++ blueprints/component/glimmer-files/__root__/__path__/__name__.ts
@@ -1,6 +1,8 @@
 import Component from '@glimmer/component';
 
-interface <%= classifiedModuleName %>Args {}
+interface <%= classifiedModuleName %>Args {
+  // define component arguments here
+}
 
 export default class <%= classifiedModuleName %> extends Component<<%= classifiedModuleName %>Args> {
   // class body definition here
