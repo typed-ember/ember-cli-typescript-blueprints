bb3bd3f76b1d1bf7d9109990d85ef9e7794452e2
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2020-11-15 18:10:31 +0000

    remove comment

--- blueprints/component/glimmer-files/__root__/__path__/__name__.ts
+++ blueprints/component/glimmer-files/__root__/__path__/__name__.ts
@@ -3,5 +3,4 @@ import Component from '@glimmer/component';
 interface <%= classifiedModuleName %>Args {}
 
 export default class <%= classifiedModuleName %> extends Component<<%= classifiedModuleName %>Args> {
-  // class body definition here
 };
