f4c3748bd008e4cb4cd10a215fb0cd68198b1e99
Bryan <bryan.crotaz@silvercurve.co.uk>
2020-11-17 16:02:28 +0000

    Update node-tests/fixtures/component/component-addon.ts
    Co-authored-by: Chris Krycho <hello@chriskrycho.com>

--- node-tests/fixtures/component/component-addon.ts
+++ node-tests/fixtures/component/component-addon.ts
@@ -2,9 +2,6 @@ import Component from '@ember/component';
 // @ts-ignore: Ignore import of compiled template
 import layout from '../templates/components/foo';
 
-export default class Foo extends Component.extend({
-  // anything which *must* be merged to prototype here
-}) {
+export default class Foo extends Component {
   layout = layout;
-  // normal class body definition here
-};
+}
