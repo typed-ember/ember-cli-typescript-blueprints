dc36cffb0105edae12337a0ece2278e8be810a85
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-12-24 19:44:59 -0500

    Update route tests.

--- blueprints/route-test/mocha-files/__root__/__testType__/__path__/__test__.ts
+++ blueprints/route-test/mocha-files/__root__/__testType__/__path__/__test__.ts
@@ -1,12 +1,15 @@
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import { setupTest } from 'ember-mocha';
+import { describeModule, it } from 'ember-mocha';
 
-describe('<%= friendlyTestDescription %>', function() {
-  setupTest();
-
-  it('exists', function() {
-    let route = this.owner.lookup('route:<%= dasherizedModuleName %>');
-    expect(route).to.be.ok;
-  });
-});
+describeModule('route:<%= moduleName %>', '<%= friendlyTestDescription %>',
+  {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  },
+  function() {
+    it('exists', function() {
+      let route = this.subject();
+      expect(route).to.be.ok;
+    });
+  }
+);
