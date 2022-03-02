7870478e7395d5187e0dcf66fcfcb2611598c990
Moacir P. de Sá Pereira <muziejus@users.noreply.github.com>
2022-03-02 12:36:07 -0500

    Update blueprints/helper/files/__root__/__collection__/__name__.js
    Co-authored-by: Chris Krycho <hello@chriskrycho.com>

--- blueprints/helper/files/__root__/__collection__/__name__.js
+++ blueprints/helper/files/__root__/__collection__/__name__.js
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function <%= camelizedModuleName %>(positional: unknown[] /*, named:Dict<unknown>*/) {
+export default helper(function <%= camelizedModuleName %>(positional: unknown[] /*, named: Record<string, unknown>*/) {
   return positional;
 });
