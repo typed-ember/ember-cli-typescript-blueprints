bb2a80563133ce33a4c050f6a7ebdea6dff2b560
Moacir P. de Sá Pereira <muziejus@users.noreply.github.com>
2022-03-02 12:36:21 -0500

    Update blueprints/service/files/__root__/__path__/__name__.ts
    Co-authored-by: Chris Krycho <hello@chriskrycho.com>

--- blueprints/service/files/__root__/__path__/__name__.ts
+++ blueprints/service/files/__root__/__path__/__name__.ts
@@ -2,7 +2,7 @@ import Service from '@ember/service';
 
 export default class <%= classifiedModuleName %>Service extends Service {}
 
-// Required for TypeScript.
+// DO NOT DELETE: this is how TypeScript knows how to look up your services.
 declare module '@ember/service' {
   interface Registry {
     '<%= dasherizedModuleName %>': <%= classifiedModuleName %>Service;
