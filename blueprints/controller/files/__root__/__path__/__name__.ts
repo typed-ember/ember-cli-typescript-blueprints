6d8202483c3b1c1ea96e51ffae3ef23c34e53d98
Moacir P. de Sá Pereira <muziejus@users.noreply.github.com>
2022-03-02 12:36:15 -0500

    Update blueprints/controller/files/__root__/__path__/__name__.ts
    Co-authored-by: Chris Krycho <hello@chriskrycho.com>

--- blueprints/controller/files/__root__/__path__/__name__.ts
+++ blueprints/controller/files/__root__/__path__/__name__.ts
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 
 export default class <%= classifiedModuleName %>Controller extends Controller {}
 
-// Required for TypeScript.
+// DO NOT DELETE: this is how TypeScript knows how to look up your controllers
 declare module '@ember/controller' {
   interface Registry {
     '<%= dasherizedModuleName %>': <%= classifiedModuleName %>Controller;
