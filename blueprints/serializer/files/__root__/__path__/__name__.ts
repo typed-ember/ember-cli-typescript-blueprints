e5d0368ec472cd45a39987a50edc998b9431fb7f
Moacir P. de Sá Pereira <muziejus@users.noreply.github.com>
2022-03-03 13:21:55 -0500

    Update blueprints/serializer/files/__root__/__path__/__name__.ts
    Co-authored-by: Chris Krycho <hello@chriskrycho.com>

--- blueprints/serializer/files/__root__/__path__/__name__.ts
+++ blueprints/serializer/files/__root__/__path__/__name__.ts
@@ -1,4 +1,10 @@
 <%= importStatement %>
 
-export default <%= baseClass %>.extend({
-});
+export default class <%= classifiedModuleName %>Serializer extends <%= baseClass %> {}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your serializers.
+declare module 'ember-data/types/registries/serializer' {
+  export default interface SerializerRegistry {
+    '<%= dasherizedModuleName %>': <%= classifiedModuleName %>;
+  }
+}
