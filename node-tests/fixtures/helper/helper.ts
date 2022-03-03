3e1dde1fdb92bdb20a5dc500717e3962a84679fc
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-03 13:33:00 -0500

    tests: Change helper fixture to use Record type instead of Dict

--- node-tests/fixtures/helper/helper.ts
+++ node-tests/fixtures/helper/helper.ts
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function fooBarBaz(positional: unknown[] /*, named:Dict<unknown>*/) {
+export default helper(function fooBarBaz(positional: unknown[] /*, named: Record<string, unknown>*/) {
   return positional;
 });
